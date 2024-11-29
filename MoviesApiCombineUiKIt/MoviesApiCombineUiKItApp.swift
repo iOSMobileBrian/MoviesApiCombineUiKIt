5b644b0b7dd567eaab684203f9e2d2eefdbf349c
Brian Surface <brian.surface@outlook.com>
2024-11-29 10:20:10 -0500

    Initial Commit

---
+++ MoviesApiCombineUiKIt/MoviesApiCombineUiKItApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApiCombineUiKItApp.swift
+//  MoviesApiCombineUiKIt
+//
+//  Created by Brian Surface on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApiCombineUiKItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
